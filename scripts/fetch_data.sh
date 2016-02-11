a300b303b420aaae52c9087ce1e73285f1d0a0e0
jaindeepali <djdeepalijain811@gmail.com>
2016-02-11 15:23:20 +0530

    Fetch html document with category listing from TechTC100 website

--- scripts/fetch_data.sh
+++ scripts/fetch_data.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-DATA_URL="http://techtc.cs.technion.ac.il/techtc100/data/techtc100_preprocessed.zip"
+DATA_URL="http://techtc.cs.technion.ac.il/techtc300/data/techtc300_preprocessed.zip"
 
 # create data directories
 mkdir ../Adler/data/
-mkdir ../Adler/data/techtc100_preprocessed/
+mkdir ../Adler/data/techtc300_preprocessed/
 echo "Data directories created"
 
 # download and unzip data
 wget $DATA_URL -P ../Adler/data/
 echo "Data downloaded"
 
-unzip ../Adler/data/techtc100_preprocessed.zip -d ../Adler/data/techtc100_preprocessed/
+unzip ../Adler/data/techtc300_preprocessed.zip -d ../Adler/data/techtc300_preprocessed/
 echo "Data unzipped"
+
+# for category listing
+LISTING_URL="http://techtc.cs.technion.ac.il/techtc300/techtc300.html"
+wget $LISTING_URL -O ../Adler/data/Tech300Categories.html
