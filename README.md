4771e4bb1cc72ee3a80c5bc495e4e6f0fa622bb2
jaindeepali <djdeepalijain811@gmail.com>
2016-02-12 13:51:19 +0530

    Add setup.py

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 ### Adler
 
 Ready-to-use text classification corpus generation from [TechTC-300 Test Collection](http://techtc.cs.technion.ac.il/techtc300/techtc100.html) .
+
+### Setup
+
+* `git clone https://github.com/jaindeepali/Adler`
+* `virtualenv .venv`
+* `source .venv/bin/activate`
+* `python setup.py install`
+* `./scripts/init.sh`
+* `/scripts/generate.py`
---
+++ setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+with open('requirements.txt') as f:
+    requirements = [l.strip() for l in f.readlines()]
+
+setup(
+    name = "Adler",
+    version = "0.0.1",
+    author = "Deepali Jain",
+    author_email = "djdeepalijain811@gmail.com",
+    description = ("Ready-to-use text classification corpus generation from [TechTC-300 Test Collection](http://techtc.cs.technion.ac.il/techtc300/techtc100.html)."),
+    license = "MIT",
+    url = "https://github.com/jaindeepali/Adler",
+    install_requires=requirements,
+    packages=['Adler'],
+    long_description=read('README.md'),
+)
