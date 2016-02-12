39ac25a6f77a95d347b4ce2cad27d10965e9a475
jaindeepali <djdeepalijain811@gmail.com>
2016-02-12 07:00:08 +0530

    Complete feature class

--- Adler/category.py
+++ Adler/category.py
@@ -7,7 +7,6 @@ class Category():
 	def __init__(self):
 		self.listing_document_path = os.path.join('data', 'Tech300Categories.html')
 		self.listing_data_object_path = os.path.join('data_objects', 'category_list.p')
-		self.listing_document = BS( open(self.listing_document_path, 'r').read() )
 		self.category_list = {}
 
 	def from_id(self, id):
@@ -18,7 +17,8 @@ class Category():
 		return self.category_list[id]
 
 	def get_list(self):
-		for row in self.listing_document.select('tr'):
+		listing_document = BS( open(self.listing_document_path, 'r').read() )
+		for row in listing_document.select('tr'):
 			for category_element in row.select('a'):
 				category_url = category_element['href']
 				category_id = category_element.text
--- Adler/feature.py
+++ Adler/feature.py
@@ -5,16 +5,30 @@ class Feature():
 
 	def __init__(self, exp):
 		self.exp = exp
+		self.listing_document_path = os.path.join('data', 'techtc300_preprocessed', exp, 'features.idx')
+		self.listing_data_object_path = os.path.join('data_objects', self.exp + '_feature_list.p')
+		self.feature_list = []
 
 	def from_id(self, id):
-		return
+		if os.path.exists(self.listing_data_object_path):
+			self.feature_list = pickle.load(open(self.listing_data_object_path))
+		else:
+			self.get_list()
+		return self.feature_list[id]
 
 	def get_list(self):
+		with open(self.listing_document_path, 'r') as f:
+			lines = [line.strip() for line in f]
+			lines = [line for line in lines if (line and line[0] != '#')]
+			self.feature_list = [l.split(' ')[1] for l in lines]
 		return
 
 	def save_list(self):
+		self.get_list()
+		pickle.dump(self.feature_list, open(self.listing_data_object_path, 'wb'))
 		return
 
 if __name__ == '__main__':
 	ob = Feature('Exp_1622_42350')
-	ob.save_list() 
+	ob.save_list()
+	print ob.from_id(23)
